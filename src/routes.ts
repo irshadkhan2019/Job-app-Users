68ae3a25608bc91aa41b6073565127cb86949015
Irshad Khan <khanirshad2019@gmail.com>
2024-06-10 11:43:55 +0530

    Health route fix

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ const BUYER_BASE_PATH = '/api/v1/buyer';
 const SELLER_BASE_PATH = '/api/v1/seller';
 
 const appRoutes = (app: Application): void => {
-  app.use('', ()=>healthRoutes());
+  app.use('', healthRoutes());
   app.use(BUYER_BASE_PATH, verifyGatewayRequest, buyerRoutes());
   app.use(SELLER_BASE_PATH, verifyGatewayRequest, sellerRoutes());
 };
